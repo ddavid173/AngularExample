8d363a60b2329c6fad3a2bba9405210666ce5ff8
ddavid173 <davidadelgado08@gmail.com>
2023-12-06 15:01:53 -0600

    Angular Base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularExample';
+  linkData: any
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('data/linkData.json').subscribe(data => {
+      this.linkData = data
+    });
+
+    // const listOfPills = document.querySelectorAll('.pill');
+
+    // setInterval(() => {
+    //   listOfPills.forEach((pill) => {
+    //     pill.classList.remove('active');
+    //   });
+      
+  }
 }
